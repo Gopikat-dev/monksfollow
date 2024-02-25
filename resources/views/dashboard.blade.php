e85ea18b61cf0d8bb84b0baa657108912f931711
handkode <handkode94@gmail.com>
2024-02-25 15:59:35 +0530

    test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
     <div class="container py-md-5 container--narrow">
     <div class="text-center">
         <h2>Hello <strong>{{ auth()->user()->email }}</strong>, your feed is empty.</h2>
-        <p class="lead text-muted">Your feed displays the latest posts from the people you follow. If you don't have any friends to follow that's okay; you can use the "Search" feature in the top menu bar to find content written by people with similar interests and then follow them.</p>
+        <p class="lead text-muted">Your page displays the latest posts from the people you follow. If you don't have any friends to follow that's okay; you can use the "Search" feature in the top menu bar to find content written by people with similar interests and then follow them.</p>
         
         <!-- Sign out button -->
         <form action="/logout" method="POST">
